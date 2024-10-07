bdaad620a6c9e8d4f8f490d90781365400e84a0e
vonxing <284934875@qq.com>
2024-10-07 09:52:40 +0800

    Update hdb.js

--- QuantumultX/hdb.js
+++ QuantumultX/hdb.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2024-10-04
 // @description  try to take over the world!
-// @author       You
+// @author       vonxing
 // @match        http://www.hdb666.com/*
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hdb666.com
